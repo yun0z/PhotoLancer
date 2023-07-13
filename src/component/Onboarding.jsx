b53b72dca63c709973b21a1ee84148868a03abe4
yun0z <shinyunjy412@naver.com>
2023-07-13 18:22:02 +0900

    [FE-03]로그인 UI

---
+++ src/component/Onboarding.jsx
@@ -0,0 +1,22 @@
+import logo1 from '../image/심볼.png';
+import logo2 from '../image/로고.png';
+
+const Onboarding = () => {
+  return (
+    <div className="onboarding">
+      <div className="ob_1">
+        <div>
+          <img src={logo1} className="symbol" />
+          <img src={logo2} className="logo" />
+        </div>
+        <button>Log in</button>
+      </div>
+      <div className="ob_2">
+        반응형 이미지 <div className="image_contain"></div>
+        <div className="image1"></div>
+        <div className="image2"></div>
+      </div>
+    </div>
+  );
+};
+export default Onboarding;
